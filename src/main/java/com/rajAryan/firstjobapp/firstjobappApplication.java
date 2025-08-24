f9f156e762965ad6486bac0d531527d7baef0b65
Prince Raj <130669466+RajjjPrince@users.noreply.github.com>
2025-08-24 22:36:52 +0530

    Add files via upload

---
+++ src/main/java/com/rajAryan/firstjobapp/firstjobappApplication.java
@@ -0,0 +1,13 @@
+package com.rajAryan.firstjobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class firstjobappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(firstjobappApplication.class, args);
+	}
+
+}
